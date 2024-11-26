2cfd855fb853926cd179806b550eb9faf2ce37ba
Davide <setti.davide89@gmail.com>
2024-11-26 15:53:15 +0100

    Use django async ORM

--- ws_lock/consumers.py
+++ ws_lock/consumers.py
@@ -1,6 +1,6 @@
 import threading
+from asgiref.sync import sync_to_async
 from collections import defaultdict
-from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from .models import Item, ItemLock, GroupTypeVisibility
 
@@ -32,8 +32,7 @@ class ItemLockConsumer(AsyncJsonWebsocketConsumer):
         print('send', threading.get_ident())
         await self.send_json(event['data'])
 
-    @database_sync_to_async
-    def connection_groups(self):
+    async def connection_groups(self):
         """Connection groups based on current user"""
         print('db-connection-2', threading.get_ident())
         user = self.scope.get('user')
@@ -41,7 +40,7 @@ class ItemLockConsumer(AsyncJsonWebsocketConsumer):
             return []
 
         visible_types = GroupTypeVisibility.objects.filter(group__in=user.groups.all()).values_list('item_type', flat=True).distinct()
-        groups = [f'type-{t}' for t in visible_types]
+        groups = [f"type-{t}" async for t in visible_types]
         print('Groups', groups, 'for user', user)
         return groups
 
@@ -53,38 +52,60 @@ class ItemLockConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         user = self.scope.get('user')
         print('connect', threading.get_ident())
-        accept = await database_sync_to_async(can_user_connect)(user)
+        accept = await sync_to_async(can_user_connect)(user)
         print('User', user, 'was accepted?', accept)
         if accept:
             await super().connect()
         else:
             await self.close()
 
-    @database_sync_to_async
-    def _update_locks(self, items):
+    async def _update_locks(self, items):
         print('db-connection-3', threading.get_ident())
         user = self.scope['user']
-        # validate items by filtering with user-group-permission
-        validated_items = list(
-            Item.objects.filter(
-                item_type__in=GroupTypeVisibility.objects.filter(group__in=user.groups.all()).values_list('item_type'),
-                id__in=items
-            ).values_list('id', flat=True)
-        )
-        # get pending locks
-        pending_locks = user.lock_items.filter(locked=True)
-        # left locks are those which item_id is no more present in received data
-        left_locks_ids = list(pending_locks.exclude(item_id__in=validated_items).values_list('id', flat=True))
-        pending_locks.exclude(item_id__in=validated_items).update(locked=False)
-        # get which locks need to be created and then create them
-        already_existing = set(ItemLock.objects.filter(locked=True, item_id__in=validated_items).values_list('item_id', flat=True))
-        ItemLock.objects.bulk_create([
-            ItemLock(user=user, item_id=item_id)
-            for item_id in validated_items if item_id not in already_existing
-        ])
-        # get active ad left locks
-        active_locks = list(pending_locks.select_related('item', 'user'))
-        left_locks = list(user.lock_items.filter(id__in=left_locks_ids).select_related('item', 'user'))
+
+        @sync_to_async
+        def _inner():
+            # validate items by filtering with user-group-permission
+            validated_items = list(
+                Item.objects.filter(
+                    item_type__in=GroupTypeVisibility.objects.filter(
+                        group__in=user.groups.all()
+                    ).values_list("item_type"),
+                    id__in=items,
+                ).values_list("id", flat=True)
+            )
+            # get pending locks
+            pending_locks = user.lock_items.filter(locked=True)
+            # left locks are those which item_id is no more present in received data
+            left_locks_ids = list(
+                pending_locks.exclude(item_id__in=validated_items).values_list(
+                    "id", flat=True
+                )
+            )
+            pending_locks.exclude(item_id__in=validated_items).update(locked=False)
+            # get which locks need to be created and then create them
+            already_existing = set(
+                ItemLock.objects.filter(
+                    locked=True, item_id__in=validated_items
+                ).values_list("item_id", flat=True)
+            )
+            ItemLock.objects.bulk_create(
+                [
+                    ItemLock(user=user, item_id=item_id)
+                    for item_id in validated_items
+                    if item_id not in already_existing
+                ]
+            )
+            # get active ad left locks
+            active_locks = list(pending_locks.select_related("item", "user"))
+            left_locks = list(
+                user.lock_items.filter(id__in=left_locks_ids).select_related(
+                    "item", "user"
+                )
+            )
+            return active_locks, left_locks
+
+        active_locks, left_locks = await _inner()
         # return every lock which needs to be sent
         print('Locks for user', user, ':', active_locks + left_locks)
         return active_locks + left_locks
@@ -99,14 +120,18 @@ class ItemLockConsumer(AsyncJsonWebsocketConsumer):
         updated_locks = await self._update_locks(items)
         await self.send_locks(updated_locks)
 
-    @database_sync_to_async
-    def _leave_locks_on_close(self):
+    async def _leave_locks_on_close(self):
         print('db-connection-4', threading.get_ident())
         user = self.scope['user']
         # fetch locked elements from database
-        user_locks = list(user.lock_items.filter(locked=True).select_related('item', 'user'))
+        user_locks = [
+            lock
+            async for lock in user.lock_items.filter(locked=True).select_related(
+                "item", "user"
+            )
+        ]
         # update locks
-        user.lock_items.filter(locked=True).update(locked=False)
+        await user.lock_items.filter(locked=True).aupdate(locked=False)
         return user_locks
 
     async def disconnect(self, code):
